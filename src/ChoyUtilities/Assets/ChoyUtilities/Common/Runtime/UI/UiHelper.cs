d834c8cfc15ca8e8a8458b54c21f2582adbe4504
DaTea83 <eugenechoy83@gmail.com>
2026-04-12 13:56:33 +0800

    Minor fix

--- src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/UI/UiHelper.cs
+++ src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/UI/UiHelper.cs
@@ -4,7 +4,13 @@ namespace ChoyUtilities {
     [RequireComponent(typeof(RectTransform))]
     public abstract class UiHelper : MonoBehaviour {
         [SerializeField] [Min(0.01f)] protected float transitionTime;
+        
+        protected RectTransform RectTransform;
 
+        protected virtual void OnValidate() {
+            RectTransform = GetComponent<RectTransform>();
+        }
+        
         public abstract void OnSpawn();
 
         public abstract float OnStartOpen();
