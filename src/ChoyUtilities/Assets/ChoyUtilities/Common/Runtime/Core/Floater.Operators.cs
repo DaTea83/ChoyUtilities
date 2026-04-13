5a174822d53e453e7365463325ad4a6989986ffb
Eugene C <eugenechoy83@gmail.com>
2026-04-13 17:09:33 +0800

    Fr this time

--- src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/Core/Floater.Operators.cs
+++ src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/Core/Floater.Operators.cs
@@ -12,9 +12,9 @@ namespace ChoyUtilities {
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Floater Add(float value) {
-            var set = new float[Values.Length + 1];
+            var set = new float[values.Length + 1];
             if (set == null) throw new FloaterException(nameof(set));
-            Array.Copy(Values, set, Values.Length);
+            Array.Copy(values, set, values.Length);
             set[^1] = value;
 
             return new Floater(set);
@@ -23,27 +23,27 @@ namespace ChoyUtilities {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Floater Add(float[] extras) {
             if (extras == null || extras == Array.Empty<float>()) return this;
-            var set = new float[Values.Length + extras.Length];
+            var set = new float[values.Length + extras.Length];
             if (set == null) throw new FloaterException(nameof(set));
 
-            for (var i = 0; i < Values.Length; i++)
-                set[i] = Values[i];
+            for (var i = 0; i < values.Length; i++)
+                set[i] = values[i];
             for (var i = 0; i < extras.Length; i++)
-                set[i + Values.Length] = extras[i];
+                set[i + values.Length] = extras[i];
 
             return new Floater(set);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Floater Add(Floater extra) {
-            if (extra.Values == null || extra.Values == Array.Empty<float>()) return this;
-            var set = new float[Values.Length + extra.Values.Length];
+            if (extra.values == null || extra.values == Array.Empty<float>()) return this;
+            var set = new float[values.Length + extra.values.Length];
             if (set == null) throw new FloaterException(nameof(set));
 
-            for (var i = 0; i < Values.Length; i++)
-                set[i] = Values[i];
-            for (var i = 0; i < extra.Values.Length; i++)
-                set[i + Values.Length] = extra.Values[i];
+            for (var i = 0; i < values.Length; i++)
+                set[i] = values[i];
+            for (var i = 0; i < extra.values.Length; i++)
+                set[i + values.Length] = extra.values[i];
 
             return new Floater(set);
         }
@@ -51,8 +51,8 @@ namespace ChoyUtilities {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int FirstMatch(float value) {
             var index = -1;
-            for (var i = 0; i < Values.Length; i++) {
-                if (!(math.abs(Values[i] - value) < EPSILON)) continue;
+            for (var i = 0; i < values.Length; i++) {
+                if (!(math.abs(values[i] - value) < EPSILON)) continue;
                 index = i;
                 break;
             }
@@ -71,22 +71,22 @@ namespace ChoyUtilities {
         /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Floater Remove(float value, bool removeAllEqual = false) {
-            if (Values == null || Values.Length == 0) return new Floater(Array.Empty<float>());
+            if (values == null || values.Length == 0) return new Floater(Array.Empty<float>());
 
             if (removeAllEqual) {
                 var keepCount = 0;
 
-                foreach (var t in Values)
+                foreach (var t in values)
                     if (math.abs(t - value) >= EPSILON)
                         keepCount++;
 
-                if (keepCount == Values.Length) return this;
+                if (keepCount == values.Length) return this;
                 if (keepCount == 0) return new Floater(Array.Empty<float>());
 
                 var set = new float[keepCount];
                 var j = 0;
 
-                foreach (var t in Values) {
+                foreach (var t in values) {
                     if (math.abs(t - value) < EPSILON) continue;
                     set[j] = t;
                     j++;
@@ -98,13 +98,13 @@ namespace ChoyUtilities {
                 var removeIndex = FirstMatch(value);
 
                 if (removeIndex < 0) return this;
-                if (Values.Length == 1) return new Floater(Array.Empty<float>());
-                var set = new float[Values.Length - 1];
+                if (values.Length == 1) return new Floater(Array.Empty<float>());
+                var set = new float[values.Length - 1];
 
                 if (removeIndex > 0)
-                    Array.Copy(Values, 0, set, 0, removeIndex);
-                if (removeIndex < Values.Length - 1)
-                    Array.Copy(Values, removeIndex + 1, set, removeIndex, Values.Length - removeIndex - 1);
+                    Array.Copy(values, 0, set, 0, removeIndex);
+                if (removeIndex < values.Length - 1)
+                    Array.Copy(values, removeIndex + 1, set, removeIndex, values.Length - removeIndex - 1);
 
                 return new Floater(set);
             }
@@ -137,18 +137,18 @@ namespace ChoyUtilities {
         /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Floater Remove(Floater value, bool removeAllEqual = false) {
-            return removeAllEqual ? RemoveAllMatches(value.Values) : RemoveFirstMatches(value.Values);
+            return removeAllEqual ? RemoveAllMatches(value.values) : RemoveFirstMatches(value.values);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private Floater RemoveFirstMatches(float[] valuesToRemove) {
-            if (Values == null || Values.Length == 0) return new Floater(Array.Empty<float>());
+            if (values == null || values.Length == 0) return new Floater(Array.Empty<float>());
             if (valuesToRemove == null || valuesToRemove.Length == 0) return this;
             
             var consumed = new bool[valuesToRemove.Length];
             var keepCount = 0;
 
-            foreach (var t in Values) {
+            foreach (var t in values) {
                 var shouldRemove = false;
 
                 for (var j = 0; j < valuesToRemove.Length; j++) {
@@ -163,14 +163,14 @@ namespace ChoyUtilities {
                 if (!shouldRemove) keepCount++;
             }
 
-            if (keepCount == Values.Length) return this;
+            if (keepCount == values.Length) return this;
             if (keepCount == 0) return new Floater(Array.Empty<float>());
 
             var set = new float[keepCount];
             consumed = new bool[valuesToRemove.Length];
             var index = 0;
 
-            foreach (var t in Values) {
+            foreach (var t in values) {
                 var shouldRemove = false;
 
                 for (var j = 0; j < valuesToRemove.Length; j++) {
@@ -193,11 +193,11 @@ namespace ChoyUtilities {
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private Floater RemoveAllMatches(float[] valuesToRemove) {
-            if (Values == null || Values.Length == 0) return new Floater(Array.Empty<float>());
+            if (values == null || values.Length == 0) return new Floater(Array.Empty<float>());
             if (valuesToRemove == null || valuesToRemove.Length == 0) return this;
             var keepCount = 0;
 
-            foreach (var t in Values) {
+            foreach (var t in values) {
                 var shouldRemove = false;
                 foreach (var t1 in valuesToRemove) {
                     if (!(math.abs(t - t1) < EPSILON)) continue;
@@ -208,13 +208,13 @@ namespace ChoyUtilities {
                 if (!shouldRemove) keepCount++;
             }
 
-            if (keepCount == Values.Length) return this;
+            if (keepCount == values.Length) return this;
             if (keepCount == 0) return new Floater(Array.Empty<float>());
 
             var set = new float[keepCount];
             var index = 0;
 
-            foreach (var t in Values) {
+            foreach (var t in values) {
                 var shouldRemove = false;
                 foreach (var t1 in valuesToRemove) {
                     if (!(math.abs(t - t1) < EPSILON)) continue;
--- src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/Core/Floater.cs
+++ src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/Core/Floater.cs
@@ -13,22 +13,22 @@ namespace ChoyUtilities {
     public partial struct Floater : IEquatable<Floater>, IComparable, IComparable<Floater>,
         IFormattable, IEnumerable<float> {
 
-        public float[] Values { get; }
+        public float[] values;
 
-        public float this[int index] => Values[index];
-        public int Length => Values.Length;
-        public bool IsCreated => Values is not null;
+        public float this[int index] => values[index];
+        public int Length => values.Length;
+        public bool IsCreated => values is not null;
 
         #region Constructors
 
-        public Floater(float[] values) { this.Values = values; }
+        public Floater(float[] values) { this.values = values; }
         public Floater(int[] values) {
             var set = new float[values.Length];
             Array.Copy(values, set, values.Length);
-            this.Values = set;
+            this.values = set;
         }
 
-        public Floater(Floater values) : this(values.Values) { }
+        public Floater(Floater values) : this(values.values) { }
         public Floater(float value) : this(new[] { value }) { }
         public Floater(float2 value) : this(new[] { value.x, value.y }) { }
         public Floater(float3 value) : this(new[] { value.x, value.y, value.z }) { }
@@ -37,7 +37,7 @@ namespace ChoyUtilities {
         public Floater(Quaternion value) : this(new[] { value.x, value.y, value.z, value.w }) { }
         public Floater(quaternion value) {
             var v = value.value;
-            Values = new[] { v.x, v.y, v.z, v.w };
+            values = new[] { v.x, v.y, v.z, v.w };
         }
         public Floater(Vector2 value) : this(new[] { value.x, value.y }) { }
         public Floater(Vector3 value) : this(new[] { value.x, value.y, value.z }) { }
@@ -55,91 +55,91 @@ namespace ChoyUtilities {
         public Floater(byte[] value) {
             var set = new float[value.Length];
             Array.Copy(value, set, value.Length);
-            Values = set;
+            values = set;
         }
         public Floater(sbyte value) : this(new int[] { value }) { }
         public Floater(sbyte[] value) {
             var set = new float[value.Length];
             Array.Copy(value, set, value.Length);
-            Values = set;
+            values = set;
         }
         public Floater(ushort value) : this(new int[] { value }) { }
         public Floater(ushort[] value) {
             var set = new float[value.Length];
             Array.Copy(value, set, value.Length);
-            Values = set;
+            values = set;
         }
         public Floater(short value) : this(new int[] { value }) { }
         public Floater(short[] value) {
             var set = new float[value.Length];
             Array.Copy(value, set, value.Length);
-            Values = set;
+            values = set;
         }
-        public Floater(bool value) { Values = new[] { value ? 1f : 0f }; }
-        public Floater(bool2 value) { Values = new[] { value.x ? 1f : 0f, value.y ? 1f : 0f }; }
+        public Floater(bool value) { values = new[] { value ? 1f : 0f }; }
+        public Floater(bool2 value) { values = new[] { value.x ? 1f : 0f, value.y ? 1f : 0f }; }
         public Floater(bool[] value) {
             var set = new float[value.Length];
             for (var i = 0; i < value.Length; i++) set[i] = value[i] ? 1f : 0f;
 
-            Values = set;
+            values = set;
         }
         public Floater(float4x4 value) {
-            Values = new float[16];
+            values = new float[16];
             var c0 = value.c0;
             var c1 = value.c1;
             var c2 = value.c2;
             var c3 = value.c3;
-            Values[0] = c0.x;
-            Values[1] = c0.y;
-            Values[2] = c0.z;
-            Values[3] = c0.w;
-            Values[4] = c1.x;
-            Values[5] = c1.y;
-            Values[6] = c1.z;
-            Values[7] = c1.w;
-            Values[8] = c2.x;
-            Values[9] = c2.y;
-            Values[10] = c2.z;
-            Values[11] = c2.w;
-            Values[12] = c3.x;
-            Values[13] = c3.y;
-            Values[14] = c3.z;
-            Values[15] = c3.w;
+            values[0] = c0.x;
+            values[1] = c0.y;
+            values[2] = c0.z;
+            values[3] = c0.w;
+            values[4] = c1.x;
+            values[5] = c1.y;
+            values[6] = c1.z;
+            values[7] = c1.w;
+            values[8] = c2.x;
+            values[9] = c2.y;
+            values[10] = c2.z;
+            values[11] = c2.w;
+            values[12] = c3.x;
+            values[13] = c3.y;
+            values[14] = c3.z;
+            values[15] = c3.w;
         }
         public Floater(Matrix4x4 value) {
-            Values = new float[16];
+            values = new float[16];
             var c0 = value.GetColumn(0);
             var c1 = value.GetColumn(1);
             var c2 = value.GetColumn(2);
             var c3 = value.GetColumn(3);
-            Values[0] = c0.x;
-            Values[1] = c0.y;
-            Values[2] = c0.z;
-            Values[3] = c0.w;
-            Values[4] = c1.x;
-            Values[5] = c1.y;
-            Values[6] = c1.z;
-            Values[7] = c1.w;
-            Values[8] = c2.x;
-            Values[9] = c2.y;
-            Values[10] = c2.z;
-            Values[11] = c2.w;
-            Values[12] = c3.x;
-            Values[13] = c3.y;
-            Values[14] = c3.z;
-            Values[15] = c3.w;
+            values[0] = c0.x;
+            values[1] = c0.y;
+            values[2] = c0.z;
+            values[3] = c0.w;
+            values[4] = c1.x;
+            values[5] = c1.y;
+            values[6] = c1.z;
+            values[7] = c1.w;
+            values[8] = c2.x;
+            values[9] = c2.y;
+            values[10] = c2.z;
+            values[11] = c2.w;
+            values[12] = c3.x;
+            values[13] = c3.y;
+            values[14] = c3.z;
+            values[15] = c3.w;
         }
         public Floater(double value) : this(new[] { (float)value }) { }
         public Floater(double[] value) {
             var set = new float[value.Length];
             Array.Copy(value, set, value.Length);
-            Values = set;
+            values = set;
         }
         public Floater(half value) : this(new[] { (float)value }) { }
         public Floater(half[] value) {
             var set = new float[value.Length];
             Array.Copy(value, set, value.Length);
-            Values = set;
+            values = set;
         }
         public Floater(char value) : this(new[] { (float)value }) { }
         public Floater(char[] value) {
@@ -149,7 +149,7 @@ namespace ChoyUtilities {
                 var j = (int)value[i];
                 set[i] = j;
             }
-            Values = set;
+            values = set;
         }
         public Floater(string value) : this(value.ToCharArray()) { }
         /// <summary>
@@ -171,20 +171,20 @@ namespace ChoyUtilities {
             set[6] = value.localScale.x;
             set[7] = value.localScale.y;
             set[8] = value.localScale.z;
-            Values = set;
+            values = set;
         }
 
         #endregion
 
         #region IEquatable, IEnumerable
 
-        public bool Equals(Floater other) { return Equals(Values, other.Values); }
+        public bool Equals(Floater other) { return Equals(values, other.values); }
         public IEnumerator<float> GetEnumerator() {
-            foreach (var t in Values)
+            foreach (var t in values)
                 yield return t;
         }
         public override bool Equals(object obj) { return obj is Floater other && Equals(other); }
-        public override int GetHashCode() { return Values is not null ? Values.GetHashCode() : 0; }
+        public override int GetHashCode() { return values is not null ? values.GetHashCode() : 0; }
 
         #endregion
 
@@ -193,9 +193,9 @@ namespace ChoyUtilities {
         public int CompareTo(Floater other) {
             float selfV = 0;
             float otherV = 0;
-            foreach (var f in Values) selfV += f;
+            foreach (var f in values) selfV += f;
 
-            foreach (var f in other.Values) otherV += f;
+            foreach (var f in other.values) otherV += f;
 
             return selfV.CompareTo(otherV);
         }
@@ -210,7 +210,7 @@ namespace ChoyUtilities {
         #region IFormattable
 
         public override string ToString() {
-            char[] set = new Floater(Values);
+            char[] set = new Floater(values);
             if (set == null || set == Array.Empty<char>()) return string.Empty;
             return set.ToString();
         }
