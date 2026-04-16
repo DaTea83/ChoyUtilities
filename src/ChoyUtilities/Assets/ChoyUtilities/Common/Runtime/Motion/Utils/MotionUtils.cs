8b605ebd88e6cb7123a5517ce97e66e45be7638d
DaTea83 <eugenechoy83@gmail.com>
2026-04-16 08:29:11 +0800

    Minor fix

--- src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/Motion/Utils/MotionUtils.cs
+++ src/ChoyUtilities/Assets/ChoyUtilities/Common/Runtime/Motion/Utils/MotionUtils.cs
@@ -1,6 +1,13 @@
-﻿namespace ChoyUtilities {
+﻿using Unity.Burst;
+
+namespace ChoyUtilities {
     
-    public static partial class HelperCollection {
-        
+    [BurstCompile]
+    public static partial class MotionCollection {
+
+        [BurstCompile]
+        public static float Evaluate(float t, EMotion motion) {
+            return 0;
+        }
     }
 }
